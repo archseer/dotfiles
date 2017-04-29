4f94ea933e41b51efe6e296ff98730dbc1eb2eea
Blaž Hrastnik <blaz@mxxn.io>
2017-04-29 22:33:41 +0900

    zsh installer & autosuggestions.

--- install.sh
+++ install.sh
@@ -13,6 +13,10 @@ for rc in *rc *profile tmux.conf gitconfig spacemacs vim/colors; do
   ln -sfv $BASE/$rc ~/.$rc
 done
 
+# zsh stuff
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 # Apply tmux settings
 tmux source-file ~/.tmux.conf
 
--- zshrc
+++ zshrc
@@ -47,7 +47,7 @@ setopt correct # only correct commands, not args
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git osx)
+plugins=(git osx zsh-autosuggestions)
 
 source $ZSH/oh-my-zsh.sh
 
