08c0894d3f820ea86dd6e016a878efa182ba548d
Blaž Hrastnik <blaz@mxxn.io>
2017-11-22 17:34:14 +0900

    Add hammerspoon config for switching between apps.

---
+++ hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.hotkey.bind({"cmd","alt","shift","ctrl"}, "t", function()
+  hs.application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind({"cmd","alt","shift","ctrl"}, "f", function()
+  hs.application.launchOrFocus("FirefoxNightly")
+end)
+
+hs.hotkey.bind({"cmd","alt","shift","ctrl"}, "c", function()
+  hs.application.launchOrFocus("Telegram")
+end)
